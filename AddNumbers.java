bd46f9df3ad03d71ec4265af2fa663574aa69c09
Lahari <lahariboinpally14@gmail.com>
2025-06-13 17:10:16 +0530

    Second commit

--- AddNumbers.java
+++ AddNumbers.java
@@ -1,7 +1,7 @@
 public class AddNumbers {
     public static void main(String[] args) {
         int number1 = 5;
-        int number2 = 10;
+        int number2 = 15;
         int sum = add(number1, number2);
         System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
     }
